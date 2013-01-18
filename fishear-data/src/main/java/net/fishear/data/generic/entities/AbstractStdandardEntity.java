4559e6d8aef077fef41db5a82f1e6290b3be1de0
ffyxrr <ffyxrr@onsemi.com>
2013-01-18 12:40:35 +0100

    typing mistake changed

---
+++ fishear-data/src/main/java/net/fishear/data/generic/entities/AbstractStdandardEntity.java
@@ -0,0 +1,14 @@
+package net.fishear.data.generic.entities;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author ffyxrr
+ * 
+ * @deprecated by typing mistake in name. {@link AbstractStandardEntity} should be used insteda
+ */
+@MappedSuperclass
+@Deprecated
+public class AbstractStdandardEntity extends AbstractStandardEntity {
+
+}
