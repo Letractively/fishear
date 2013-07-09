884d2a3bb2cf6d456a005b81d00c7de2fe0cf526
ffyxrr <ffyxrr@onsemi.com>
2013-07-09 13:37:52 +0200

    restrictions test started

---
+++ fishear-data/src/test/java/net/fishear/data/generic/data/EntityWithInterval.java
@@ -0,0 +1,39 @@
+package net.fishear.data.generic.data;
+
+import java.util.Date;
+
+public class EntityWithInterval {
+
+	private Date validFrom;
+	
+	private Date validTo;
+
+	/**
+	 * @return the validFrom
+	 */
+	public Date getValidFrom() {
+		return validFrom;
+	}
+
+	/**
+	 * @param validFrom the validFrom to set
+	 */
+	public void setValidFrom(Date validFrom) {
+		this.validFrom = validFrom;
+	}
+
+	/**
+	 * @return the validTo
+	 */
+	public Date getValidTo() {
+		return validTo;
+	}
+
+	/**
+	 * @param validTo the validTo to set
+	 */
+	public void setValidTo(Date validTo) {
+		this.validTo = validTo;
+	}
+	
+}
