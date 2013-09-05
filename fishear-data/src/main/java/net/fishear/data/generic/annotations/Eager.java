d70a01636df2fed172cf7e42010987b0abf831ae
ffyxrr <ffyxrr@onsemi.com>
2013-09-05 09:57:17 +0200

    javadocs corrected

--- fishear-data/src/main/java/net/fishear/data/generic/annotations/Eager.java
+++ fishear-data/src/main/java/net/fishear/data/generic/annotations/Eager.java
@@ -10,10 +10,10 @@ import net.fishear.data.generic.query.QueryConstraints;
 import net.fishear.data.generic.services.GenericService;
 
 /**
- * getter annotation, that instructs {@link GenericService} to load field's content programmatically after entity is loaded (aither in list method or read methos).
+ * getter annotation, that instructs {@link GenericService} to load field's content programmatically after entity is loaded (either in list method or read methods).
  * This is done by access of this field's value, in case {@link Collection} calls {@link Collection#size()} method.
  * Another way to set eagerly loaded fields is class level annotation {@link EagerLoads}.
- * Eagerly loaded properties list may also be set for each query using {@link QueryConstraints} (which overrides both annotations - uf you use empty list or array for the query, no eager loading will be applied).
+ * Eagerly loaded properties list may also be set for each query using {@link QueryConstraints} (which overrides both annotations - if you use empty list or array for the query, no eager loading will be applied).
  * 
  * @author terber
  *
