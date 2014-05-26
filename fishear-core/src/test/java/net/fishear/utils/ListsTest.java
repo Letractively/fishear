e177c6e4476101513870c7c196e67f55d32a80dd
ffyxrr <ffyxrr@onsemi.com>
2014-05-26 15:53:37 +0200

    util methods added

--- fishear-core/src/test/java/net/fishear/utils/ListsTest.java
+++ fishear-core/src/test/java/net/fishear/utils/ListsTest.java
@@ -1,9 +1,12 @@
 package net.fishear.utils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class ListsTest {
@@ -13,6 +16,12 @@ public class ListsTest {
 		private String val1;
 		
 		private Double val2;
+		
+		private int numInt; 
+		
+		private long numLong;
+		
+		private Integer numInt2;
 
 		/**
 		 * @return the val1
@@ -40,6 +49,48 @@ public class ListsTest {
 		 */
 		public void setVal2(Double val2) {
 			this.val2 = val2;
+		}
+
+		/**
+		 * @return the numInt
+		 */
+		public int getNumInt() {
+			return numInt;
+		}
+
+		/**
+		 * @param numInt the numInt to set
+		 */
+		public void setNumInt(int numInt) {
+			this.numInt = numInt;
+		}
+
+		/**
+		 * @return the numLong
+		 */
+		public long getNumLong() {
+			return numLong;
+		}
+
+		/**
+		 * @param numLong the numLong to set
+		 */
+		public void setNumLong(long nulLong) {
+			this.numLong = nulLong;
+		}
+
+		/**
+		 * @return the numInt2
+		 */
+		public Integer getNumInt2() {
+			return numInt2;
+		}
+
+		/**
+		 * @param numInt2 the numInt2 to set
+		 */
+		public void setNumInt2(Integer numInt2) {
+			this.numInt2 = numInt2;
 		}
 	}
 	
@@ -89,6 +140,72 @@ public class ListsTest {
 		for(ListIt it : list) {
 			assertEquals(it.getVal2(), null);
 		}
+	}
+	
+	@Test
+	public void sumTest() {
+		List<ListIt> list = newist();
+		list.get(0).setNumLong(10);
+		list.get(2).setNumLong(20);
+		list.get(4).setNumLong(30);
+		assertEquals(Lists.sum(list, "numLong"), new BigDecimal(60));
+		
+		list.get(1).setNumInt2(20);
+		list.get(3).setNumInt2(40);
+		list.get(5).setNumInt2(60);
+		assertEquals(Lists.sum(list, "numInt2"), new BigDecimal(120));
+
+	}
+	
+	@Test
+	public void toMapTest() {
+		List<ListIt> list = newist();
+		int cnt = 0;
+		for(ListIt it : list) {
+			it.setVal2(((double)cnt + 1) * 10);
+			it.setVal1("THEKEY_"+cnt);
+			cnt++;
+		}
+		
+		Map<String, ListIt> map1 = Lists.toKeyEntityMap(list, "val1");
+		Map<Double, ListIt> map2 = Lists.toKeyEntityMap(list, "val2");
+
+		assertEquals(map1.get("THEKEY_4").getVal1(), "THEKEY_4");
+		assertEquals(map2.get(20D).getVal1(), "THEKEY_1");
+		
+	}
+	
+	@Test(expectedExceptions=IllegalStateException.class)
+	public void toMapNullKeyTest() {
+		List<ListIt> list = newist();
+		int cnt = 0;
+		for(ListIt it : list) {
+			it.setVal2(((double)cnt + 1) * 10);
+			it.setVal1("THEKEY_"+cnt);
+			cnt++;
+			if(cnt > 2) {
+				break;
+			}
+		}
+		Lists.toKeyEntityMap(list, "val1");
+		
+	}
+	
+	@Test(expectedExceptions=IllegalStateException.class)
+	public void toMapDuplityTest() {
+		List<ListIt> list = newist();
+		int cnt = 0;
+		for(ListIt it : list) {
+			it.setVal2(((double)cnt + 1) * 10);
+			it.setVal1("THEKEY_"+cnt);
+			if(cnt == 4) {
+				it.setVal1("THEKEY_"+(cnt - 1));
+			}
+			cnt++;
+		}
+
+		Lists.toKeyEntityMap(list, "val1");
+
 	}
 	
 	
