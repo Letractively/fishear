a84d48de8327c4871fcab1919adf373e6739c868
raterwork <raterwork@localhost>
2014-06-08 17:31:14 +0200

    refactored / renamed

---
+++ fishear-t5/src/main/java/net/fishear/web/t5/base/AbstractDetailComponent.java
@@ -0,0 +1,52 @@
+package net.fishear.web.t5.base;
+
+import net.fishear.data.generic.entities.EntityI;
+import net.fishear.data.generic.entities.EntitySourceI;
+import net.fishear.web.t5.data.PagingDataSource;
+
+/**
+ * Component that constructs {@link PagingDataSource} for given service, suit for a grid.
+ * Also contains 
+ * 
+ * @author ffyxrr
+ *
+ * @param <T>
+ */
+public abstract class
+	AbstractDetailComponent<T extends EntityI<Long>> 
+extends 
+	GenericDetailComponent<T>
+implements 
+	EntitySourceI<T>
+{
+
+	public T getEntity() {
+		return super.getEntity();
+	}
+
+	protected Object onDelete(Long id) {
+		return super.onDelete(id);
+	}
+
+	protected Object onDelete(Object id) {
+		try {
+			return super.onDelete(Long.parseLong(id.toString()));
+		} catch(Exception ex) {
+			log.error("Exception during 'onDelete' method call", ex);
+			return getReturn();
+		}
+	}
+
+	protected Object onDetail(Long id) {
+		return super.onDetail(id);
+	}
+
+	protected Object onDetail(Object id) {
+		try {
+			return super.onDetail(Long.parseLong(id.toString()));
+		} catch(Exception ex) {
+			log.error("Exception during 'onDetail' method call", ex);
+			return getReturn();
+		}
+	}
+}
