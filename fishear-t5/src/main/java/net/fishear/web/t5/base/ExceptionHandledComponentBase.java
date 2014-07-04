a6f8c330ce192540e89ac55899e76216197bbfe6
ffyxrr <ffyxrr@onsemi.com>
2014-07-04 09:13:57 +0800

    error handling component base name changed to more intuitive (old one deprecated)

---
+++ fishear-t5/src/main/java/net/fishear/web/t5/base/ExceptionHandledComponentBase.java
@@ -0,0 +1,12 @@
+package net.fishear.web.t5.base;
+
+/**
+ * for backward compatibility, will be removed in next release.
+ * 
+ * @author ffyxrr
+ * @deprecated 
+ *
+ */
+public class ExceptionHandledComponentBase extends ComponentBaseHandled {
+
+}
