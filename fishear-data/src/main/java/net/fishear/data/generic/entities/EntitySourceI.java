9cd0fda46faf0e063f708a2501b4443104464acf
ffyxrr <ffyxrr@onsemi.com>
2014-07-15 10:14:46 +0200

    javadoc added

--- fishear-data/src/main/java/net/fishear/data/generic/entities/EntitySourceI.java
+++ fishear-data/src/main/java/net/fishear/data/generic/entities/EntitySourceI.java
@@ -11,8 +11,20 @@ package net.fishear.data.generic.entities;
  */
 public interface EntitySourceI<T extends EntityI<?>> {
 
+	/**
+	 * Implementation of this interface manages persistent entity (for example in session). 
+	 * Value that is set by {@link #setEntity(EntityI)} must be returned by any subseqwuent call of {@link #getEntity()}.
+	 * The exception is setEntity(null), when subsequent getEntity() call returns new entity instance.
+	 * 
+	 * @return entity instance (either new instance - not persisted, or loaded from persistent storage). <b>NEVER RETURNS NULL</b>. If null has been set as entity by previous setEntity() call,  creates new entity instance. 
+	 */
 	T getEntity();
 	
+	/**
+	 * sets required entity. 
+	 * 
+	 * @param entity the new entity value. May be null = new entity instance is created during subsequent {@link #getEntity()} call. 
+	 */
 	void setEntity(T entity);
  	
 	
